8bd520a27652f277f4134fcef74400b85a1e4bb6
Kevin Klopfenstein <klopfensteinsoftware@gmail.com>
2019-02-27 22:36:00 -0500

    Modified to allow one-step initialization
    - moved swift endpoint registration to before apache foreground start
    - Updated README to reflect changes

--- swift/bin/launch.sh
+++ swift/bin/launch.sh
@@ -1,15 +1,40 @@
 #!/bin/sh
+
 service rsyslog start
 service rsync start
 service memcached start
-# set up storage
-#mkdir -p /swift/nodes/1
-#ln -s /swift/nodes/1 /srv/1
-#mkdir -p /srv/1/node/sdb1 /var/run/swift
-#/usr/bin/sudo /bin/chown -R swift:swift /swift/nodes /etc/swift /var/run/swift /srv/1 
-#/usr/bin/sudo -u swift /swift/bin/remakerings
 service swift-proxy start
 service swift-container start
 service swift-account start
 service swift-object start
+
+if [ -z "$KS_SWIFT_PUBLIC_URL" ]; then
+    echo "KS_SWIFT_PUBLIC_URL is required"
+    exit 1
+fi
+
+CONFIG_PUB_URL=$(cat /opt/SWIFT_INIT)
+
+if [ "$KS_SWIFT_PUBLIC_URL" = "$CONFIG_PUB_URL" ]; then
+    echo "$KS_SWIFT_PUBLIC_URL endpoint already configured."
+else
+    echo "Configuring $KS_SWIFT_PUBLIC_URL endpoint."
+
+    echo "Starting apache."
+    apachectl start
+    echo "Apache started."
+
+    openstack endpoint create --region RegionOne object-store internal http://127.0.0.1:8080/v1/KEY_%\(tenant_id\)s
+    openstack endpoint create --region RegionOne object-store admin http://127.0.0.1:8080/v1
+    openstack endpoint create --region RegionOne object-store public ${KS_SWIFT_PUBLIC_URL}/v1/KEY_%\(tenant_id\)s
+
+    echo "Stopping apache."
+    apachectl stop
+    echo "Apache stopped."
+
+    echo "$KS_SWIFT_PUBLIC_URL" > /opt/SWIFT_INIT
+fi
+
+echo "Starting apache in foreground."
+
 apachectl -DFOREGROUND
